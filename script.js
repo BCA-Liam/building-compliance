49aa804a5b85e8c2d64db7e5f1febc45635c0066
BCA-Liam <bca@consultant.com>
2025-07-30 05:47:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// This file can be used for any interactive JavaScript functionality.
+// For example, smooth scrolling for internal links:
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
